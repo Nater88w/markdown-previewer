84ba6ed5efa2dd7d7866a860b6d955025ce77595
Nater88w <nwarnock88@gmail.com>
2023-12-14 19:32:15 -0500

    rough-build

--- markdown-previewer/src/editor.js
+++ markdown-previewer/src/editor.js
@@ -1,11 +1,13 @@
 
-export default function Editor({set}){
+export default function Editor({set,text}){
+    
     function type(e){
-        set(e.target.value)
-    }
+        set(e.target.value);
+       }
     return(
-        <div id='editor'>
-            <textarea onInput={type} rows='10' cols='40'></textarea>
+        <div className="edit">
+            <div className="header"><b>Edit</b></div>
+            <textarea id='editor' defaultValue={text} onInput={type} rows='10' cols='40'></textarea>
         </div>
     )
 }
